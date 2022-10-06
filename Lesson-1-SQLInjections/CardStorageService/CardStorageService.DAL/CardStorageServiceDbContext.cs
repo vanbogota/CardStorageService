7fd991c567a5f27cec5fbceb9f9cbc268a9b73f0
Ivan-Dell <vanbog@mail.ru>
2022-10-06 20:02:01 +0300

    homework of lesson 2

--- Lesson-1-SQLInjections/CardStorageService/CardStorageService.DAL/CardStorageServiceDbContext.cs
+++ Lesson-1-SQLInjections/CardStorageService/CardStorageService.DAL/CardStorageServiceDbContext.cs
@@ -13,6 +13,9 @@ namespace CardStorageService.DAL
         public virtual DbSet<Client> Clients { get; set; }
         public virtual DbSet<Card> Cards { get; set; }
 
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<AccountSession> AccountSessions { get; set; }
+
         public CardStorageServiceDbContext(DbContextOptions options) : base(options) { }
 
     }
---
+++ Lesson-1-SQLInjections/CardStorageService/CardStorageService/Utils/PasswordUtils.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CardStorageService.Utils
+{
+    public class PasswordUtils
+    {
+        private const string SecretKey = "1234567890";
+
+        public static (string passwordSalt, string passwordHash) CreatePasswordHash(string password)
+        {
+            #region generate random salt 
+              
+            byte[] buffer = new byte[16];
+            RNGCryptoServiceProvider secureRandom = new RNGCryptoServiceProvider();
+            secureRandom.GetBytes(buffer);
+            #endregion
+
+            #region create hash & salt
+
+            string passwordSalt = Convert.ToBase64String(buffer);
+            string passwordHash = GetPasswordHash(password, passwordSalt);
+
+            #endregion
+            
+            return (passwordSalt, passwordHash);
+        }
+
+        public static bool VerifyPassword(string password, string passwordSalt,
+            string passwordHash)
+        {
+            return GetPasswordHash(password, passwordSalt) == passwordHash;
+        }
+
+        public static string GetPasswordHash(string password, string passwordSalt)
+        {
+            
+            password = $"{password}~{passwordSalt}~{SecretKey}";
+            byte[] buffer = Encoding.UTF8.GetBytes(password);
+
+            SHA512 sha512 = new SHA512Managed();
+            byte[] passwordHash = sha512.ComputeHash(buffer);
+
+            return Convert.ToBase64String(passwordHash);
+        }
+    }
+}
